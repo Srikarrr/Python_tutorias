e4574a3e708d49d7867547fc2774c8bee6722403
Shanmukha Koppala <shakoppa@publicisgroupe.net>
2024-08-20 09:28:24 +0530

    print

--- secondprogram.py
+++ secondprogram.py
@@ -56,3 +56,69 @@ str=str.capitalize()
 print(str)
 
 print(str.replace("python","javascript"))
+
+light = "pink"
+if(light == "red"):
+  print("stop")
+elif(light == "green"):
+  print("go")
+elif(light == "yellow"):
+  print("look")
+else:
+  print("light is broken")  
+
+
+age = 14
+if(age>=18):
+  print("can vote")
+else:
+  print("cannot vote")  
+
+marks = input("enter student marks: ")
+if(marks >= 90):
+  grade = "A"
+elif(marks>=80 and marks<90):
+  grade = "B"
+elif(marks>=70 and marks <80):
+  grade = "C"
+else:
+  grade = "D"
+print("grade of the student ->",grade)
+
+#nesting if under if
+if(age >= 18):
+   if(age>=80):
+      print("cannot drive")
+   else:
+      print("can drive")
+else:
+   print("cannot drive")           
+
+
+
+num=int(input("Enter Number"))
+rem = num % 2
+if(rem == 0):
+  print("EVEN")
+else:
+  print("ODD")  
+
+#print biggest of 3 numbers
+  
+a = int(input("enter first number  : "))
+b = int(input("enter second number : "))
+c = int(input("enter third number :  "))
+
+if(a>=b and a>=c):
+  print("first number is bigger",a)
+elif(b>=c):
+  print("second number is largest",b)
+else:
+  print("third is largest",c)
+
+x=int(input("Enter Number"))
+  
+if(x % 7 == 0):
+  print("multiple of 7")
+else:
+  print("not a multiple") 
