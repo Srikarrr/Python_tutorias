f8a47ec8c0eea63f3fc0725b32190dd2c0c6006f
Shanmukha Koppala <shakoppa@publicisgroupe.net>
2025-02-22 18:30:27 +0530

    added docker files

--- 24-01-2025/Docker/HelloWorld/app.py
+++ 24-01-2025/Docker/HelloWorld/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
 import os
-app = Flask(__name__)
+import time
+import redis
+
+app   = Flask(__name__)
+cache = redis.Redis(host='redis',port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    try:
+        cache.reset_retry_count()
+        return cache.incr('hits')
+    except redis.exceptions.connectionError as exc:
+           if retries == 0:
+                raise exc
+           retries -=1
+           time.sleep(0.5)
 
 @app.route('/',methods=['GET'])
 def home():
     return "Home World"
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello Krish! I have been seen {} times.\n'.format(count) 
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
